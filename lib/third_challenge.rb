e03a84ede73055c666c084ea87241dbc22d4b6f3
Sara Walker <sarawall1187@gmail.com>
2018-12-18 22:18:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -41,7 +41,12 @@ def third_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => []
+      :hero_friends => [
+        {:name => "Benvolio", :age => "14", :attitude => "worried"}
+        {:name => "Mercution", :age => "18", :attitude =>
+          "hot-headed"
+        }
+        ]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
