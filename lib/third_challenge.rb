396eb861bca08e64a6e6fb287ebc09b743798049
Sara Walker <sarawall1187@gmail.com>
2018-12-18 22:11:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -28,9 +28,8 @@ def third_challenge
       :heroine => {
         :name => "Juliet",
         :age => "15",
-        :status => "alive",
+        :status => "alive"},
       :heroine_friends => []
-      }
-   }
+    }
   }
 end
